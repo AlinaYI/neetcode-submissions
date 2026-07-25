c4df0e6160fbfe7c3600a8c4eab56915333e02ac
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-07-25 00:26:18 -0700

    Add: min-cost-to-connect-points - submission-4

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-4.py
@@ -0,0 +1,46 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [0]*n
+
+    def find(self, node):
+        curr = node
+        while curr != self.parent[curr]:
+            self.parent[curr] = self.parent[self.parent[curr]]
+            curr = self.parent[curr]
+        return curr
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+
+        if root_u == root_v:
+            return False
+        
+        if self.size[root_u] < self.size[root_v]:
+            root_u, root_v = root_v, root_u
+        
+        self.parent[root_v] = root_u
+        self.size[root_u] += self.size[root_v]
+
+        return True
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        edges = []
+        for i in range(len(points)):
+            x1, y1 = points[i]
+            for j in range(i + 1, len(points)):
+                x2, y2 = points[j]
+
+                dist = abs(x1-x2) + abs(y1 - y2)
+                edges.append((dist, i, j))
+
+        dsu = DSU(len(points))
+        edges.sort()
+        res = 0
+        for dist, u, v in edges:
+            if dsu.union(u,v):
+                res += dist
+        return res
