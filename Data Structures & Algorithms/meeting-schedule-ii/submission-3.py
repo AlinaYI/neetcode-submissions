aef381cedf04f152a695cb412facc6987c7cff4d
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-08-03 23:30:53 -0700

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,23 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        
+        intervals.sort(key=lambda key:key.start)
+        minHeap = []
+        for interval in intervals:
+            start = interval.start
+            end = interval.end
+            
+            if minHeap and start >= minHeap[0]:
+                heapq.heappop(minHeap)
+            heapq.heappush(minHeap, end)
+        return len(minHeap)
+            
+                
