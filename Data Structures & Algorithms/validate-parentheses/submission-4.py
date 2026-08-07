849effdbbc559097069ea373914d479718023514
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-08-07 15:33:29 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        hashmap={
+            ")":"(",
+            "]":"[",
+            "}":"{"
+        }
+
+        stack = []
+        for c in s:
+            if c in "([{":
+                stack.append(c)
+            else:
+                if stack and stack[-1] == hashmap[c]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
