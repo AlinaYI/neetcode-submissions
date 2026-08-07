2b1b0bc735b476eeea83cdeb6a6bd3c4e05166ab
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-08-07 15:30:04 -0700

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        if len(t) > len(s):
+            return ""
+        
+        count = Counter(t)
+        stillNeed = len(t)
+        left = 0
+        res = ""
+
+        for right in range(len(s)):
+            char = s[right]
+            if char in count and count[char] > 0:
+                stillNeed -= 1
+            
+            count[char] -= 1
+            while stillNeed == 0:
+                window = s[left:right+1]
+                if not res or len(res) > len(window):
+                    res = window
+                
+                leftChar = s[left]
+                count[leftChar] += 1
+                if count[leftChar] > 0:
+                    stillNeed += 1
+                left += 1
+        return res
