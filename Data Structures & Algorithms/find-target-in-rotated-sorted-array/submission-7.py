fac8b7fc297a8174bdb90dce3026a6612eb994be
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-08-10 22:44:22 -0700

    Add: find-target-in-rotated-sorted-array - submission-7

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        # 这道题就是要找到target
+        left, right = 0, len(nums)-1
+        while left <= right:
+
+            mid = left + (right-left)//2
+
+            if nums[mid] == target:
+                return mid
+
+            # 说明split的点在右边，左边是肯定是
+            # [小->大]
+            elif nums[mid] > nums[-1]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # 如果split的点在左边，那么右边就是顺序的
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1
