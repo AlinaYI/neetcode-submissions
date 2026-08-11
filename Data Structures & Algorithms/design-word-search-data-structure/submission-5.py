214173700648cc21796c73181eca4b12b3691973
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-08-11 00:17:12 -0700

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.py
@@ -0,0 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+    
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end = True
+
+    def search(self, word: str) -> bool:
+        
+        def dfs(node, idx):
+            if idx == len(word):
+                return node.end
+            
+            currChar = word[idx]
+            if currChar == ".":
+                for letter in node.children.values():
+                    if dfs(letter, idx+1):
+                        return True
+                return False
+            
+            if currChar not in node.children:
+                return False
+            
+            return dfs(node.children[currChar], idx+1)
+
+
+        return dfs(self.root, 0)
+        
