7036155124e18380de0735d194befdebce2ac06b
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-08-11 10:19:07 -0700

    Add: valid-tree - submission-10

---
+++ Data Structures & Algorithms/valid-tree/submission-10.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        q = deque([0])
+        seen = {0}
+        while q:
+            curr = q.popleft()
+            
+            for nei in graph[curr]:
+                if nei not in seen:
+                    seen.add(nei)
+                    q.append(nei)
+        return len(seen) == n
