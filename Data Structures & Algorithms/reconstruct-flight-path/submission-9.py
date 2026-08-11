7bf77caca24ec035a01f430c5b69880c5450b95a
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-08-11 10:52:15 -0700

    Add: reconstruct-flight-path - submission-9

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        
+        graph = defaultdict(list)
+        for start, end in sorted(tickets, reverse=True):
+            graph[start].append(end)
+        
+        stack = ["JFK"]
+        res = []
+        while stack:
+            curr = stack[-1]
+
+            if graph[curr]:
+                nextDst = graph[curr].pop()
+                stack.append(nextDst)
+            else:
+                res.append(stack.pop())
+        return res[::-1]
+        
