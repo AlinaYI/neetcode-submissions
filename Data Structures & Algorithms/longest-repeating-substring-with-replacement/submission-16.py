2469e3dde4bb8314a17998781ecf5ae9b1d4cb4e
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-08-18 22:25:04 -0700

    Add: longest-repeating-substring-with-replacement - submission-16

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-16.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = defaultdict(int)
+        maxFreq = 0
+        left, right = 0, 0
+        res = 0
+        while right < len(s):
+            count[ s[right] ] += 1
+            maxFreq = max(maxFreq, count[s[right]])
+
+            while right-left+1 - maxFreq > k:
+                count[s[left]] -= 1
+                left += 1
+                
+            res = max(res, right-left+1)
+            right += 1
+        return res
