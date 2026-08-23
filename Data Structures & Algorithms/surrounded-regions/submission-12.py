056f32659544859cd38e1abfa6583ee30d1b3a3a
Alina DING <109413460+AlinaYI@users.noreply.github.com>
2026-08-23 13:24:39 -0700

    Add: surrounded-regions - submission-12

---
+++ Data Structures & Algorithms/surrounded-regions/submission-12.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        
+        def dfs(i,j):
+            board[i][j] = 'D'
+            for di, dj in directions:
+                ni,nj = i+di, j+dj
+                if 0<=ni<row and 0<=nj<col and board[ni][nj] == 'O':
+                    board[ni][nj] = 'D'
+                    dfs(ni,nj)
+
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        row, col = len(board), len(board[0])
+        for i in range(row):
+            if board[i][0] == 'O':
+                dfs(i, 0)
+            if board[i][col-1] == 'O':
+                dfs(i, col-1)
+
+        for j in range(col):
+            if board[0][j] == 'O':
+                dfs(0, j)
+            if board[row-1][j] == 'O':
+                dfs(row-1, j)
+        
+        # mark connected to D
+        # go throught the map, 
+        # change all D to O, left O to X
+        for i in range(row):
+            for j in range(col):
+                if board[i][j] == 'D':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
+        return 
